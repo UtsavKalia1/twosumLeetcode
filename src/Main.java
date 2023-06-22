911477b2e92988ed6ac4ffd3483eac1bab0318c5
UtsavKalia1 <utsavkalia66@gmail.com>
2023-06-21 22:29:07 -0400

    starting leetcode exercise

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+
+// given an array of integers nums
+//and an integer target,
+// return indices of the two numbers such that
+//they add up to target
+
+//you may assume that each input would have
+//exactly one solution and you may not
+//use the the same element twice
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
