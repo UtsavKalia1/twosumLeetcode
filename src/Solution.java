03b2e2f6ace5760df672a50ffd6c9f6ab374a77a
UtsavKalia1 <utsavkalia66@gmail.com>
2023-07-03 22:55:20 -0400

    doing same with hashmap

--- src/Solution.java
+++ src/Solution.java
@@ -1,14 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
 import static java.lang.System.*;
 
 public class Solution {
     public int[] twoSum(int[] nums, int target){
+        Map<Integer, Integer> complements = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            for (int j = i+1; j < nums.length; j++) {
-                if(nums[i] + nums[j] == target){
-                    return new int[]{i,j};
-
-                }
-            }
+//            for (int j = i+1; j < nums.length; j++) {
+//                if(nums[i] + nums[j] == target){
+//                    return new int[]{i,j};
+//
+//                }
+//            }
+            //now doing this with hashmap as
+            //it is much faster
+          Integer complementindex =   complements.get(nums[i]);
+          if (complementindex != null){
+              return new int[]{i,complementindex};
+          }
+          complements.put(target-nums[i],i);
         }
         return nums;
     }
